392a21697998117cfb99aa17a75f095e15b3ead9
Hyeonji-Ahn <ahj2875@gmail.com>
2023-07-19 16:56:51 -0400

    delete actual eye & refer to first frame

--- eyeblack.py
+++ eyeblack.py
@@ -1,70 +1,64 @@
-# import required libraries
-import cv2
-import numpy as np
-import os
-from PIL import Image
+#Eye Black
+
+
+# Directory containing the images
+inDir_black = mainDir + "\Cropped"
+outDir_black = mainDir+"\EyeBlacked"
+
+if not os.path.exists(outDir_black):
+    os.makedirs(outDir_black)
+
 def black_eyes(img):
    eyes=[]
    # convert to grayscale of each frames
    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
    # read the haarcascade to detect the faces in an image
-   face_cascade = cv2.CascadeClassifier(r"C:\Users\panda\Downloads\eyetracker\haarcascade_frontalface_default.xml")
-   eye_cascade = cv2.CascadeClassifier(r"C:\Users\panda\Downloads\eyetracker\haarcascade_eye_tree_eyeglasses.xml")
+   eye_cascade = cv2.CascadeClassifier(r"C:\Users\ahj28\Desktop\Python\haarcascade_eye_tree_eyeglasses.xml")
 
    # detects faces in the input image
-   faces = face_cascade.detectMultiScale(gray_frame, 1.3, 4)
-   print('Number of detected faces:', len(faces))
-   # loop over the detected faces
-   for (x,y,w,h) in faces:
-      roi_gray = gray_frame[y:y+h, x:x+w]
-      roi_color = img[y:y+h, x:x+w]
-      
       # detects eyes of within the detected face area (roi)
-      eyes.extend(eye_cascade.detectMultiScale(roi_gray))
+   eyes.extend(eye_cascade.detectMultiScale(gray_frame))
+   print(eyes)
       # draw a rectangle around eyes
-   return eyes, roi_color
+   return eyes
       #for (ex,ey,ew,eh) in eyes:
 
-# def crop_images(images, eyes, roi_color):
-#    cropped_images = []
-#    i = 0
-#    # Crop each image based on the box coordinates
-#    for image in images:
-#       i +=1
-  
-# # rectangle box yay 
-#    for (ex,ey,ew,eh) in eyes:
-#       cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,255),2)
-#    cropped_images.append(image)
+def crop_images(images):
+    cropped_images = []
+    i = 0
+    # Crop each image based on the box coordinates
+    for image in images:
+        i +=1
+# taking half of the width:
+        for i in range(eyes[0][0], eyes[0][0]+eyes[0][2]):
+            for j in range(eyes[0][1], eyes[0][1]+eyes[0][3]):
+                image[j, i] = (1,1,1)
+        for i in range(eyes[1][0], eyes[1][0]+eyes[1][2]):
+            for j in range(eyes[1][1], eyes[1][1]+eyes[1][3]):
+                image[j, i] = (1,1,1)
+        cropped_images.append(image)
+    return cropped_images
 
-#    return cropped_images
-
-# # Directory containing the images
-#  # Replace with the actual directory path
-# #outDir = r"C:\Users\ahj28\Desktop\Python\SampleOutput430sec\Cropped"
-# inDir = r"C:\Users\panda\Downloads\ShirleyFPS"
-# outDir = r"C:\Users\panda\OneDrive\Desktop\FacialRecTests\7.14.23\FinalImages"
-
-# if not os.path.exists(outDir):
-#     os.makedirs(outDir)
-
-# # Get a list of image file names in the directory
-# image_files = [f for f in os.listdir(inDir) if f.endswith('.png')]
-
-# # Load the images
-# images = [cv2.imread(os.path.join(inDir, image_file)) for image_file in image_files]
+# Get a list of image file names in the directory
+image_files = [f for f in os.listdir(inDir_black) if f.endswith('.png')]
 
 
-# # Crop the series of images
-# eyes, roi_color = black_eyes(images[0])
-# cropped_images = crop_images(images, eyes, roi_color)
 
-# def returnZero(currentI, desiredNum):
-#     ans = "";
-#     for i in range (desiredNum - len(str(currentI))):
-#         ans += "0"
-#     return ans
+# Load the images
+images = [cv2.imread(os.path.join(inDir_black, image_file)) for image_file in image_files]
 
-# # Display and save the cropped images
-# for i, cropped_image in enumerate(cropped_images):
-#     cv2.imwrite(os.path.join(outDir, returnZero(i+1,len(str(len(cropped_images)))) + f"{i+1}.png"), cropped_image)
+
+# Crop the series of images
+eyes = black_eyes(images[0])
+cropped_images = crop_images(images)
+#cropped_images = crop_images(images)
+
+def returnZero(currentI, desiredNum):
+    ans = "";
+    for i in range (desiredNum - len(str(currentI))):
+        ans += "0"
+    return ans
+
+#Display and save the cropped images
+for i, cropped_image in enumerate(cropped_images):
+    cv2.imwrite(os.path.join(outDir_black, returnZero(i+1,len(str(len(cropped_images)))) + f"{i+1}.png"), cropped_image)
